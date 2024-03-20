eb0ce3d84b2b8c41f37018e93d50bc98f48db67d
Dmitry <79690865+bymagadan@users.noreply.github.com>
2024-03-20 23:33:12 +0700

    Update main.js
    обновление

--- main.js
+++ main.js
@@ -49,8 +49,56 @@ function compileCode() {
     updateOutput();
 }
 
-// Update the output when the page loads
+function loadVideo() {
+    var url = document.getElementById("youtube-url").value;
+    var videoContainer = document.getElementById("video-player");
+    var videoId = getYouTubeId(url);
+    if (videoId) {
+
+        var videoHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>';
+        videoContainer.innerHTML = videoHTML;
+        var video = videoContainer.querySelector('iframe');
+        video.style.height = '200px';
+    } else {
+        videoContainer.innerHTML = "<p>Введите валидную ссылку</p>";
+    }
+}
+
+
+function getYouTubeId(url) {
+    var regExp = /^.*(youtu\.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    if (match && match[2].length === 11) {
+        return match[2];
+    } else {
+        return null;
+    }
+}
+
+document.getElementById("html-code").addEventListener("input", updateOutput);
+document.getElementById("css-code").addEventListener("input", updateOutput);
+
+function updateOutput() {
+    var htmlCode = document.getElementById("html-code").value;
+    var cssCode = "<style>" + document.getElementById("css-code").value + "</style>";
+    var outputFrame = document.getElementById("output-frame").contentDocument;
+    outputFrame.open();
+    outputFrame.write(htmlCode + cssCode);
+    outputFrame.close();
+}
+function loadVideo() {
+    var url = document.getElementById("youtube-url").value;
+    var videoContainer = document.getElementById("video-player");
+    var videoId = getYouTubeId(url);
+    if (videoId) {
+        videoContainer.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + videoId + '" frameborder="0" allowfullscreen></iframe>';
+    } else {
+        videoContainer.innerHTML = "<p>Введите валидную ссылку</p>";
+    }
+}
+
+
 window.onload = updateOutput;
 
-// Update the output when the input code changes
+
 document.getElementById('input-code').addEventListener('input', updateOutput);
